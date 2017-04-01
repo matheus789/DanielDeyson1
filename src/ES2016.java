66cf9d252d92600d97b043b3fb1c4fbb2c41c938
matheus789 <raimundo.matheus@dce.ufpb.br>
2017-03-31 23:04:46 -0300

    Update ES2016.java

--- src/ES2016.java
+++ src/ES2016.java
@@ -1,4 +1,11 @@
+import javax.swing.JOptionPane;
 
 public class ES2016 {
+  public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String message = "GGWP";
+		JOptionPane.showMessageDialog(null, message);
+
+	}
 
 }
